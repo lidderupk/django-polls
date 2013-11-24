7e707335156722bc07f9994d5c9fb388d0ea7247
U-ZINFANDEL\ulidder <ulidder@zinfandel.(none)>
2013-11-24 10:59:46 -0800

    initial commit

---
+++ polls/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from polls.models import Question, Choice
+
+
+# class QuestionAdmin(admin.ModelAdmin):
+# 	fields = ['pub_date', 'question_text']
+
+
+class ChoiceInline(admin.StackedInline):
+	model = Choice
+	extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+	list_display = ('question_text','pub_date','was_published_recently')
+	fieldsets = [
+		(None, {'fields': ['question_text']}),
+		('Date information',{'fields': ['pub_date']}),
+	]
+	inlines = [ChoiceInline]
+	list_filter = ['pub_date']
+	search_fields= ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
